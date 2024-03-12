8aedc18522b52c44ee9de7ce3a86646b7fddb3d3
alvarofeher <alvarofeherargullos@gmail.com>
2024-03-12 09:09:17 +0100

    init test

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class AnswerBlock extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+
+    );
+  }
+
+
+}
+
+class ScrollableTextCard extends StatelessWidget {
+  final String text;
+
+  const ScrollableTextCard({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox( // Wrap SingleChildScrollView with SizedBox
+          height: 250.0, // Set desired fixed height
+          child: SingleChildScrollView(
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Multiple choice cloze"),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+ScrollableTextCard(text: "After years studying North America's black bears in the (0) .... way wildlife biologist Luke Robertson felt no closer to understanding the creatures. He realised that he had to (1) .... their trust. Abandoning scientific detachment, he took the daring step of forming relationships with the animals, bringing them food to gain their acceptance.The (2) .... this has given him into their behaviour has allowed him to dispel certain myths about bears. (3) .... to popular belief, he contends that bears do not (4) .... as much for fruit as previously supposed. He also (5) .... that they are ferocious. He says that people should not be (6) .... by behaviour such as swatting paws on the ground as this is a defensive rather than an aggresive act.However, Robertson is no sentimentalist. After devoting years of his life to bears, he is under no (7) .... about their feelings for him. It is clear that their interest in him does not (8) .... beyond the food he brings.")
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
